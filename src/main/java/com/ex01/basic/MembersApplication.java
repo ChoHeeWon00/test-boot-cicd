88bed1d6235d3098fa958b623ab3e5f1ade7e885
choheewon <choheewon@hanmail.net>
2026-06-12 15:53:21 +0900

    1111

--- src/main/java/com/ex01/basic/MembersApplication.java
+++ src/main/java/com/ex01/basic/MembersApplication.java
@@ -2,10 +2,16 @@ package com.ex01.basic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MembersApplication {
+public class MembersApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(MembersApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MembersApplication.class);
 	}
 }
